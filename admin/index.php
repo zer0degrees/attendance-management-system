97b349f78949095e88839f697be04f0698fa7a90
Okomba Mugoma <mugoma99@gmail.com>
2022-08-27 13:32:42 +0300

    Fix: fix static files;

--- admin/index.php
+++ admin/index.php
@@ -1,28 +1,29 @@
-<?php 
+<?php
 
-	include '../include/controller.php';
-	$session_username = $_SESSION['user_name'];
-	if(empty($_SESSION['user_name'])) {
-		header("location:../index.php");
-	}
+include '../include/controller.php';
+$session_username = $_SESSION['user_name'];
+if (empty($_SESSION['user_name'])) {
+	header("location:../index.php");
+}
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Admin</title>
 
 	<!-- Font Awesome -->
-  	<link rel="stylesheet" href="public/css/font-awesome.min.css">
-  	<!-- Bootstrap core CSS -->
-  	<link rel="stylesheet" href="public/css/bootstrap.min.css">
-  	<!-- Material Design Bootstrap -->
-  	<link rel="stylesheet" href="public/css/mdb.min.css">
-  	<!-- Sweetalert -->
-  	<script src="public/js/sweetalert.min.js"></script>
-  	<!-- Custom styles -->
-  	<link rel="stylesheet" href="public/css/style.css">
+	<link rel="stylesheet" href="/public/css/font-awesome.min.css">
+	<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" href="/public/css/bootstrap.min.css">
+	<!-- Material Design Bootstrap -->
+	<link rel="stylesheet" href="/public/css/mdb.min.css">
+	<!-- Sweetalert -->
+	<script src="/public/js/sweetalert.min.js"></script>
+	<!-- Custom styles -->
+	<link rel="stylesheet" href="/public/css/style.css">
 </head>
 
 <body class="fixed-sn light-blue-skin">
@@ -55,13 +56,13 @@
 				</li>
 				<li class="nav-item dropdown">
 					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						<i class="fa fa-user" ></i>
+						<i class="fa fa-user"></i>
 						<span class="clearfix d-none d-sm-inline-block">Account</span>
 					</a>
-						<div class="dropdown-menu dropdown-menu-right" arial-labelledby="navbarDropdownMenuLink">
-							<a class="dropdown-item" href="#"> My Profile</a>
-							<a class="dropdown-item" href="#"> Logout</a>
-						</div>
+					<div class="dropdown-menu dropdown-menu-right" arial-labelledby="navbarDropdownMenuLink">
+						<a class="dropdown-item" href="#"> My Profile</a>
+						<a class="dropdown-item" href="#"> Logout</a>
+					</div>
 				</li>
 			</ul>
 			<!-- Links -->
@@ -70,7 +71,7 @@
 
 		<!-- Sidebar Navigation -->
 		<div id="slide-out" class="slide-nav fixed sn-bg-4 ">
-			<ul class="custom-scrollbar list-unstyled" >
+			<ul class="custom-scrollbar list-unstyled">
 				<!-- Logo -->
 				<li class="logo-sn waves-effect">
 					<div class="text-center">
@@ -82,18 +83,18 @@
 				<!-- Logo -->
 				<!-- Search Form -->
 				<li>
-					<form class="search-form" role="search" >
-						<div class="md-form my-0 waves-light" >
-							<input type="text" name="form-control py-2" placeholder="Search..." >
-							
+					<form class="search-form" role="search">
+						<div class="md-form my-0 waves-light">
+							<input type="text" name="form-control py-2" placeholder="Search...">
+
 						</div>
-						
+
 					</form>
 				</li>
 				<!-- Search Form -->
 				<!-- Side Navigation Links -->
 				<li>
-					<ul class="collapsible collapsible-accordion" >
+					<ul class="collapsible collapsible-accordion">
 						<li>
 							<a href="index.php">
 								<i class="fa fa-home"></i> Dashboard
@@ -166,7 +167,7 @@
 
 			</ul>
 			<!-- Mask -->
-			<div class="sidenav-bg mask-strong" > </div>
+			<div class="sidenav-bg mask-strong"> </div>
 			<!-- Mask -->
 		</div>
 		<!-- Sidebar Navigation -->
@@ -176,12 +177,13 @@
 
 	<!-- SCRIPTS -->
 	<!-- JQuery -->
-	<script type="text/javascript" src="public/js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="/public/js/jquery-3.3.1.min.js"></script>
 	<!-- Bootstrap tooltips -->
-	<script type="text/javascript" src="public/js/popper.min.js"></script>
+	<script type="text/javascript" src="/public/js/popper.min.js"></script>
 	<!-- Bootstrap core JavaScript -->
-	<script type="text/javascript" src="public/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="/public/js/bootstrap.min.js"></script>
 	<!-- MDB core JavaScript -->
-	<script type="text/javascript" src="public/js/mdb.min.js"></script>
+	<script type="text/javascript" src="/public/js/mdb.min.js"></script>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -15,15 +15,15 @@ if (isset($_SESSION['user_name'])) {
 	<title> Student Attendance Management System</title>
 
 	<!-- Font Awesome -->
-  	<link rel="stylesheet" href="public/css/font-awesome.min.css">
-  	<!-- Bootstrap core CSS -->
-  	<link rel="stylesheet" href="public/css/bootstrap.min.css">
-  	<!-- Material Design Bootstrap -->
-  	<link rel="stylesheet" href="public/css/mdb.min.css">
-  	<!-- Sweetalert -->
-  	<script src="public/js/sweetalert.min.js"></script>
-  	<!-- Custom styles -->
-  	<link rel="stylesheet" href="public/css/style.css">
+	<link rel="stylesheet" href="/public/css/font-awesome.min.css">
+	<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" href="/public/css/bootstrap.min.css">
+	<!-- Material Design Bootstrap -->
+	<link rel="stylesheet" href="/public/css/mdb.min.css">
+	<!-- Sweetalert -->
+	<script src="/public/js/sweetalert.min.js"></script>
+	<!-- Custom styles -->
+	<link rel="stylesheet" href="/public/css/style.css">
 
 </head>
 
@@ -48,27 +48,27 @@ if (isset($_SESSION['user_name'])) {
 								<select name="user_type" class="browser-default custom-select mb-4">
 									<option value="" disabled>Choose User Type</option>
 									<option value="admin" selected>Admin</option>
-									<option value="hod" >HOD</option>
+									<option value="hod">HOD</option>
 									<option value="staff">Lecturer</option>
 								</select>
 
-								<input type="text" id="username" name="username" value="<?php echo $username; ?>" class="form-control mb-4" placeholder="Username" required /> 
+								<input type="text" id="username" name="username" value="<?php echo $username; ?>" class="form-control mb-4" placeholder="Username" required />
 								<?php echo $usernameErr ?>
 
-								<input type="password" id="password" name="password" value="" class="form-control mb-4" placeholder="Password" required /> 
+								<input type="password" id="password" name="password" value="" class="form-control mb-4" placeholder="Password" required />
 								<?php echo $passwordErr ?>
 
 								<div class="d-flex">
 									<div>
 										<div class="custom-control custom-checkbox">
-											<input type="checkbox" class="custom-control-input"id="defaultLoginFormRemember" />
+											<input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember" />
 											<label class="custom-control-label" id="defaultLoginFormRemember">Remember me</label>
 										</div>
 									</div>
 								</div>
 
 								<!-- Sign in Button -->
-									<button class="btn btn-info btn-block my-4" type="submit" name="login">Sign in </button>
+								<button class="btn btn-info btn-block my-4" type="submit" name="login">Sign in </button>
 								<!-- Sign in Button -->
 
 							</form>
@@ -76,26 +76,27 @@ if (isset($_SESSION['user_name'])) {
 						</div>
 					</div>
 					<div class="col-md-3">
-						
+
 					</div>
 				</div>
 				<!-- Grid row -->
 			</section>
 		</div>
-			<!-- Countainer-fluid -->
+		<!-- Countainer-fluid -->
 	</main>
 	<!-- Main Layout -->
 
 
 	<!-- SCRIPTS -->
 	<!-- JQuery -->
-	<script type="text/javascript" src="public/js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="/public/js/jquery-3.3.1.min.js"></script>
 	<!-- Bootstrap tooltips -->
-	<script type="text/javascript" src="public/js/popper.min.js"></script>
+	<script type="text/javascript" src="/public/js/popper.min.js"></script>
 	<!-- Bootstrap core JavaScript -->
-	<script type="text/javascript" src="public/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="/public/js/bootstrap.min.js"></script>
 	<!-- MDB core JavaScript -->
-	<script type="text/javascript" src="public/js/mdb.min.js"></script>
+	<script type="text/javascript" src="/public/js/mdb.min.js"></script>
 
 </body>
+
 </html>
